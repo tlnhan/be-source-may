268e39d0d6bfee1371a8e9077b9e1a1964d2f166
TLN Nhăn <truonglapnhan1309@gmail.com>
2023-11-06 23:49:56 +0700

    create-proc dvt, check proc, update other procs

--- routers/list/list_dvt.routes.js
+++ routers/list/list_dvt.routes.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const mssql = require("mssql");
+const mssqlConfig = require("../../configs/database");
 
 router.get("/", async (req, res, next) => {
   try {
-    const result = await mssql.query("SELECT * FROM DM_DVT");
+    const result = await mssql.query("EXEC sp_LayDanhSachDVT");
+
     res.status(200).json(result.recordset);
   } catch (error) {
-    res.status(500).json({ error: "Error while getting list-dvt." });
+    res.status(500).json({ error: "Lỗi khi lấy danh sách đơn vị tính." });
   }
 });
 
 router.post("/", async (req, res, next) => {
   try {
     const {
-      MaDVT,
       TenDVT,
       VietTat,
       GhiChu,
@@ -25,41 +26,31 @@ router.post("/", async (req, res, next) => {
       Update_User,
     } = req.body;
 
-    const result = await mssql.query(
-      `
-                INSERT INTO DM_DVT (
-                    [MaDVT],
-                    [TenDVT],
-                    [VietTat],
-                    [GhiChu],
-                    [TamNgung],
-                    [Createdate],
-                    [Create_User],
-                    [Update_Date],
-                    [Update_User]
-                ) VALUES (
-                  '${MaDVT}',
-                  N'${TenDVT}',
-                  '${VietTat}',
-                  N'${GhiChu}',
-                  '${TamNgung}',
-                  '${Createdate}',
-                  N'${Create_User}',
-                  '${Update_Date}',
-                  N'${Update_User}'
-                )
-                  `
-    );
-    res.status(200).json(result.recordset);
+    const pool = await mssql.connect(mssqlConfig);
+
+    const request = new mssql.Request(pool);
+
+    request.input("TenDVT", mssql.NVarChar(150), TenDVT);
+    request.input("VietTat", mssql.NVarChar(50), VietTat);
+    request.input("GhiChu", mssql.NVarChar(150), GhiChu);
+    request.input("TamNgung", mssql.Bit, TamNgung);
+    request.input("Createdate", mssql.SmallDateTime, Createdate);
+    request.input("Create_User", mssql.NVarChar(50), Create_User);
+    request.input("Update_Date", mssql.SmallDateTime, Update_Date);
+    request.input("Update_User", mssql.NVarChar(50), Update_User);
+
+    await request.execute("sp_ThemDVT");
+
+    res.status(200).json({ message: "Đơn vị tính đã được thêm." });
   } catch (error) {
-    res.status(500).json({ error: "Error while adding list-dvt." });
+    res.status(500).json({ error: "Lỗi khi thêm đơn vị tính." });
   }
 });
 
 router.put("/", async (req, res, next) => {
   try {
     const {
-      MaDVT,
+      DVT_ID,
       TenDVT,
       VietTat,
       GhiChu,
@@ -70,35 +61,42 @@ router.put("/", async (req, res, next) => {
       Update_User,
     } = req.body;
 
-    const result = await mssql.query(
-      `
-                      UPDATE DM_DVT
-                      SET TenDVT = '${TenDVT}', 
-                      VietTat = '${VietTat}', 
-                      GhiChu = '${GhiChu}', 
-                      TamNgung = '${TamNgung}', 
-                      Createdate = '${Createdate}', 
-                      Create_User = N'${Create_User}', 
-                      Update_Date = '${Update_Date}', 
-                      Update_User = '${Update_User}',
-                      WHERE MaDVT = '${MaDVT}'
-                  `
-    );
-    res.status(200).json(result.recordset);
+    const pool = await mssql.connect(mssqlConfig);
+
+    const request = new mssql.Request(pool);
+
+    request.input("DVT_ID", mssql.Int, DVT_ID);
+    request.input("TenDVT", mssql.NVarChar(150), TenDVT);
+    request.input("VietTat", mssql.NVarChar(50), VietTat);
+    request.input("GhiChu", mssql.NVarChar(150), GhiChu);
+    request.input("TamNgung", mssql.Bit, TamNgung);
+    request.input("Createdate", mssql.SmallDateTime, Createdate);
+    request.input("Create_User", mssql.NVarChar(50), Create_User);
+    request.input("Update_Date", mssql.SmallDateTime, Update_Date);
+    request.input("Update_User", mssql.NVarChar(50), Update_User);
+
+    await request.execute("sp_CapNhatDVT");
+
+    res.status(200).json({ message: "Đơn vị tính đã được cập nhật." });
   } catch (error) {
-    res.status(500).json({ error: "Error while updating list-dvt." });
+    res.status(500).json({ error: "Lỗi khi cập nhật đơn vị tính." });
   }
 });
 
 router.delete("/", async (req, res, next) => {
   try {
-    const { MaDVT } = req.body;
-    await mssql.query(`DELETE FROM DM_DVT WHERE MaDVT = '${MaDVT}'`);
-    res.status(200).json({
-      message: `The list-dvt with MaDVT: ${MaDVT} has been deleted.`,
-    });
+    const { DVT_ID } = req.body;
+
+    const pool = await mssql.connect(mssqlConfig);
+
+    const request = new mssql.Request(pool);
+    request.input("DVT_ID", mssql.Int, DVT_ID);
+
+    await request.execute("sp_XoaDVT ");
+
+    res.status(200).json({ message: "Xóa đơn vị tính thành công." });
   } catch (error) {
-    res.status(500).json({ error: "Error while deleting list-dvt." });
+    res.status(500).json({ error: "Lỗi khi xóa đơn vị tính." });
   }
 });
 
