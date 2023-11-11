f210dd618863de6d6f22eed050f3783a2738d207
sorios0810 <hunganhnguyen2001@gmail.com>
2023-11-11 12:33:41 +0700

    cập nhật lại api cập nhật sl

--- routers/manufacture/updatequantity.routes.js
+++ routers/manufacture/updatequantity.routes.js
@@ -19,37 +19,23 @@ router.get("/", async (req, res, next) => {
 router.post("/", async (req, res, next) => {
     try {
         const {
-            SanLuong_Id,
             PhanCong_Id,
-            SoLuong,
-            KhoaSanLuong,
-            NgayKhoaSanLuong,
-            NguoiKhoaSanLuong,         
+            SoLuong,     
         } = req.body;
 
         const pool = await mssql.connect(mssqlConfig);
         const request = new mssql.Request(pool);
         const requestCheck = new mssql.Request(pool);
         requestCheck.input("PhanCongId", mssql.Int, PhanCong_Id);
-        requestCheck.input("SanLuongId", mssql.BigInt, SanLuong_Id);
         const result = await requestCheck.query("SELECT SoLuong FROM PhanCongSanXuat WHERE PhanCong_Id = @PhanCongId");
-        const resultQuantity = await requestCheck.query("SELECT COUNT(*) AS Count FROM CapNhatSanLuong WHERE SanLuong_Id = @SanLuongId");
-        const allocatedQuantity = result.recordset[0].SoLuong;
-        const checkExsit = resultQuantity.recordset[0].Count;
+        const allocatedQuantity = result.recordset[0].SoLuong;      
 
         if (SoLuong > allocatedQuantity) {
             return res.status(400).json({ error: "Số lượng cập nhật không thể lớn hơn số lượng phân công." });
         }   
-
-        if (checkExsit > 0) {
-            return res.status(400).json({ error: "Bị trùng sản lượng. Vui lòng thử lại" });
-        }
-        request.input("SanLuongId", mssql.BigInt, SanLuong_Id);
+      
         request.input("PhanCongId", mssql.Int, PhanCong_Id);
         request.input("SoLuong", mssql.Int, SoLuong);
-        request.input("KhoaSanLuong", mssql.Bit, KhoaSanLuong);
-        request.input("NgayKhoaSanLuong", mssql.SmallDateTime, NgayKhoaSanLuong);
-        request.input("NguoiKhoaSanLuong", mssql.Int, NguoiKhoaSanLuong);
         await request.execute("sp_ThemTongHopLuong");
        
         res.status(200).json({ message: "Thêm thành công!" });
@@ -64,10 +50,7 @@ router.put("/", async (req, res, next) => {
         const {
             SanLuong_Id,
             PhanCong_Id,
-            SoLuong,
-            KhoaSanLuong,
-            NgayKhoaSanLuong,
-            NguoiKhoaSanLuong,           
+            SoLuong,           
         } = req.body;
 
         const pool = await mssql.connect(mssqlConfig);
@@ -85,9 +68,6 @@ router.put("/", async (req, res, next) => {
         request.input("SanLuongId", mssql.BigInt, SanLuong_Id);
         request.input("PhanCongId", mssql.Int, PhanCong_Id);
         request.input("SoLuong", mssql.Int, SoLuong);
-        request.input("KhoaSanLuong", mssql.Bit, KhoaSanLuong);
-        request.input("NgayKhoa", mssql.SmallDateTime, NgayKhoaSanLuong);
-        request.input("NguoiKhoaSanLuong", mssql.Int, NguoiKhoaSanLuong);
         await request.execute("sp_CapNhatTongHopLuong");
 
         res.status(200).json({ message: "Cập nhật thành công!" });
