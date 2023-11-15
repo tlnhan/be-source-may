1d2edc6ac08b5efac267dc5a5258e50514821024
TLN Nhăn <truonglapnhan1309@gmail.com>
2023-11-15 20:13:42 +0700

    fix-code-block-and-open-quantity

--- routers/manufacture/block_or_open_quantity.routes.js
+++ routers/manufacture/block_or_open_quantity.routes.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const mssql = require("mssql");
+const mssqlConfig = require("../../configs/database"); 
 
 router.get("/", async (req, res, next) => {});
 
 router.post("/", async (req, res, next) => {
     try {
-        const { action, ngaysanluong, PhanCong_Id, User_Id } = req.body;
+        const { Action, PhanCong_Id, User_Id } = req.body;
 
-        const pool = await mssql.connect(mssqlCon);
+        const pool = await mssql.connect(mssqlConfig);
         const request = new mssql.Request(pool);
 
-        request.input("action", mssql.NVarChar(50), action);
-        request.input("ngaysanluong", mssql.Date, ngaysanluong);
+        request.input("Action", mssql.NVarChar(50), Action);
         request.input("PhanCong_Id", mssql.Int, PhanCong_Id);
         request.input("User_Id", mssql.NChar(10), User_Id);
 
-        const result = await request.execute("sp_KhoaCapNhatSanLuong");
+        await request.execute("sp_KhoaCapNhatSanLuong");
 
         res.status(200).json({ message: "Thao tác sản lượng thành công." });
     } catch (error) {
