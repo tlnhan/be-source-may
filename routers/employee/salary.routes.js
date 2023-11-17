c0f7561f10d3dfc6be007d73a906e4dfad090758
TLN Nhăn <truonglapnhan1309@gmail.com>
2023-11-17 20:45:26 +0700

    update salary employee

--- routers/employee/salary.routes.js
+++ routers/employee/salary.routes.js
@@ -5,21 +5,15 @@ const mssqlConfig = require("../../configs/database");
 
 router.get("/", async (req, res, next) => {
   try {
-      const { Thang, Nam } = req.query; // Lấy tháng và năm từ query params
+    const pool = await mssql.connect(mssqlConfig);
+    const request = new mssql.Request(pool);
 
-      const pool = await mssql.connect(mssqlConfig);
-      const request = new mssql.Request(pool);
+    const result = await request.execute("sp_TinhLuongNhanVien");
 
-      request.input("Thang", mssql.TinyInt, Thang);
-      request.input("Nam", mssql.SmallInt, Nam);
-      request.output("Luong", mssql.Float);
-
-      const result = await request.execute("sp_TinhLuongNhanVien");
-
-      res.status(200).json(result.recordset);
+    res.status(200).json(result.recordset);
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Lỗi khi tính lương cho nhân viên." });
+    console.error(error);
+    res.status(500).json({ error: "Lỗi khi tính lương cho nhân viên." });
   }
 });
 
